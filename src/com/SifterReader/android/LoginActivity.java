b419f52775f2627072cd019aeeadc9f44c9d0750
Mark Mikofski <bwanamarko@yahoo.com>
2012-01-16 16:22:49 -0800

    remove ProjectActivity, test SifterReader.apk, change cancelKey to cancel in login.xml, complete LoginActivity class, move URL connection to SifterReader in loadProject method

--- src/com/SifterReader/android/LoginActivity.java
+++ src/com/SifterReader/android/LoginActivity.java
@@ -1,28 +1,68 @@
 package com.SifterReader.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends Activity {
+	
+	private EditText mDomainName;
+    private EditText mAccessKey;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
-	}
-
-	private String[] getKeys() {
-
-		String[] keys = new String[2];
-
+		
 		// capture our View elements
-		EditText domainEntry = (EditText) findViewById(R.id.domainEntry);
-		EditText accessKeyEntry = (EditText) findViewById(R.id.accessKeyEntry);
+		mDomainName = (EditText) findViewById(R.id.domainEntry);
+		mAccessKey = (EditText) findViewById(R.id.accessKeyEntry);
+		Button saveButton = (Button) findViewById(R.id.saveKey);
+		Button cancelButton = (Button) findViewById(R.id.cancel);
 
-		keys[0] = domainEntry.getText().toString();
-		keys[1] = accessKeyEntry.getText().toString();
-		return keys;
+		Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String domain = extras.getString(SifterReader.DOMAIN);
+            String accessKey = extras.getString(SifterReader.ACCESS_KEY);
+
+            if (domain != null) {
+                mDomainName.setText(domain);
+            }
+            if (accessKey != null) {
+            	mAccessKey.setText(accessKey);
+            }
+        }
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+        	
+        	// anonymous inner class
+        	public void onClick(View view) {
+        		Bundle bundle = new Bundle();
+
+        		bundle.putString(SifterReader.DOMAIN, mDomainName.getText().toString());
+        		bundle.putString(SifterReader.ACCESS_KEY, mAccessKey.getText().toString());
+        		
+        		Intent mIntent = new Intent();
+        		mIntent.putExtras(bundle);
+        		setResult(RESULT_OK, mIntent);
+        		finish();
+        	}
+
+        });
+        
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+        	
+        	// anonymous inner class
+        	public void onClick(View view) {
+        		Intent mIntent = new Intent();
+        		setResult(RESULT_CANCELED, mIntent);
+        		finish();
+        	}
+
+        });
+
 	}
-
 }
--- src/com/SifterReader/android/SifterReader.java
+++ src/com/SifterReader/android/SifterReader.java
@@ -23,17 +23,32 @@ package com.SifterReader.android;
  *      
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class SifterReader extends Activity {
 	
-	public static final int LOGIN_ID = Menu.FIRST; // int id for login menu option
-	public static final int ACTIVITY_LOGIN = 0; // int id for intent result
-
+	// Constants
+	public static final int LOGIN_ID = Menu.FIRST; // id for login menu option
+	public static final int ACTIVITY_LOGIN = 0; // id for intent result
+	public static final String DOMAIN = "domain"; // key to unbundle domain from intent extras
+	public static final String ACCESS_KEY = "accessKey"; // key to unbundle access key from intent extras
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -66,19 +81,106 @@ public class SifterReader extends Activity {
     	startActivityForResult(intent, ACTIVITY_LOGIN);
     }
     
-//	public void onClickProjects(View v) {
-//		
-//		String[] keys = new String[2];
-//		keys = getKeys();
-//
-//		// Create an Intent to launch Projects Activity
-//		Intent intent = new Intent(this, ProjectsActivity.class);
-//		intent.putExtra("domain", keys[0]);
-//		intent.putExtra("accessKey", keys[1]);
-//
-//		startActivity(intent);
-//	}
+    /** Determine activity by result code 
+     * 	ACTIVITY_LOGIN unbundle domain & access key */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        Bundle extras = intent.getExtras();
+        switch(requestCode) {
+        case ACTIVITY_LOGIN:
+        	String domain = extras.getString(SifterReader.DOMAIN);
+        	String accessKey = extras.getString(SifterReader.ACCESS_KEY);
+        	loadProjects(domain, accessKey);
+        	break;
+        }
+    }
+	
+    private void loadProjects(String domain, String accessKey) {
+		// capture our View elements
+		TextView issues = (TextView) findViewById(R.id.projects);
+		
+		URL sifter;
+		try {
+			// create URL object to SifterAPI
+			sifter = new URL("https://" + domain
+					+ ".sifterapp.com/api/projects");
+			URLConnection sifterConnection = null;
+			try {
+				// open connection to SifterAPI
+				sifterConnection = sifter.openConnection();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				issues.append(e.toString());
+			}
+			// add Access Key to header request
+			sifterConnection.setRequestProperty("X-Sifter-Token", accessKey);
 
-	
-	
+			// add Accept: application/json to header - also not necessary
+			sifterConnection.addRequestProperty("Accept", "application/json");
+
+			// create buffer and open input stream
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					sifterConnection.getInputStream()));
+			String inputLine;
+			StringBuilder x = new StringBuilder();
+
+			while ((inputLine = in.readLine()) != null) {
+				x.append(inputLine);
+			}
+			in.close();
+
+			// initialize "projects" JSONObject from string
+			JSONObject projects = new JSONObject(x.toString());
+
+			// array of projects
+			JSONArray projectArray = projects.getJSONArray("projects");
+			int numberProjects = projectArray.length();
+			JSONObject[] p = new JSONObject[numberProjects];
+
+			// projects
+			for (int i = 0; i < numberProjects; i++) {
+				p[i] = projectArray.getJSONObject(i);
+			}
+
+			// project field names
+//			JSONArray fieldNames = p[0].names();
+//			int numKeys = p[0].length();
+			SifterProj[] proj = new SifterProj[numberProjects];
+			for (int j = 0; j < numberProjects; j++) {
+				proj[j] = new SifterProj(p[j].getString("api_url"),
+						p[j].getString("archived"),
+						p[j].getString("api_issues_url"),
+						p[j].getString("milestones_url"),
+						p[j].getString("api_milestones_url"),
+						p[j].getString("api_categories_url"),
+						p[j].getString("issues_url"), p[j].getString("name"),
+						p[j].getString("url"),
+						p[j].getString("api_people_url"),
+						p[j].getString("primary_company_name"));
+				issues.append("************ project: " + (j + 1) + " ************\n");
+//				for (int i = 0; i < numKeys; i++) {
+//					issues.append(fieldNames.getString(i) + " : "
+//							+ p[j].getString(fieldNames.getString(i)) + "\n");
+//				}
+				issues.append(proj[j].name + "\n");
+			}
+
+			// issues.append(y);
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			issues.append(e.toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			issues.append(e.toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			issues.append(e.toString());
+		}
+    }
 }
