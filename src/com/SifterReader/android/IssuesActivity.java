4e4492a4ec6b5cf1dfbd4c61e1c7ac93f8714891
Mark Mikofski <bwanamarko@yahoo.com>
2012-02-02 04:08:10 -0800

    can edit page number and both buttons up top
    gives more screen, don't have to use weight,
    only one relative layout, don't need linear layout
    separate layouts and weighting required for footer

--- src/com/SifterReader/android/IssuesActivity.java
+++ src/com/SifterReader/android/IssuesActivity.java
@@ -34,7 +34,7 @@ public class IssuesActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.issues_footer);
+		setContentView(R.layout.issues_header);
 		registerForContextMenu(getListView());
 		
 		TextView pageTotal = (TextView)findViewById(R.id.page_total);
