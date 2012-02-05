04cfc934bfd8f0264bf840a80be7207512524419
Mark Mikofski <bwanamarko@yahoo.com>
2012-02-05 04:24:57 -0800

    add menu item to set issues per page * add layout for perpage dialog * add call back to set and refresh perpage after dialog dismissed * fix bug with goto page

--- src/com/SifterReader/android/IssuesActivity.java
+++ src/com/SifterReader/android/IssuesActivity.java
@@ -10,10 +10,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,8 +36,10 @@ public class IssuesActivity extends ListActivity {
 	public static final String PREVIOUS_PAGE_URL = "previous_page_url";
 	public static final String GOTO_PAGE = "page";
 	public static final String PER_PAGE = "per_page";
+	public static final int MAX_PER_PAGE = 25;
 	public static final int SETTINGS_ID = Menu.FIRST;
 	public static final int EXIT_ID = Menu.FIRST + 1;
+	static final int NUMBER_DIALOG_ID = 0;
 	
 	// Members
 	private SifterHelper mSifterHelper;
@@ -43,6 +48,7 @@ public class IssuesActivity extends ListActivity {
 	private JSONObject[] mAllIssues;
 	private int mTotalPages;
 	private int mPage;
+	private int mPerPage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +76,7 @@ public class IssuesActivity extends ListActivity {
 					getIssues();
 					mPage = mIssues.getInt(PAGE);
 					mTotalPages = mIssues.getInt(TOTAL_PAGES);
+					mPerPage = mIssues.getInt(PER_PAGE);
 					pageNumber.setText(String.valueOf(mPage));
 					pageTotal.setText(" / " + String.valueOf(mTotalPages));
 
@@ -91,7 +98,15 @@ public class IssuesActivity extends ListActivity {
 						// anonymous inner class
 						public void onClick(View view) {
 							EditText pageNumber = (EditText)findViewById(R.id.page_number);
-							loadIssuesPage(pageNumber);
+							int newPage = 0;
+							try {
+							newPage = Integer.valueOf(pageNumber.getText().toString());
+							} catch (NumberFormatException e) {
+								pageNumber.setText(String.valueOf(mPage));
+								e.printStackTrace();
+								return;
+							}
+							loadIssuesPage(newPage);
 						}
 					});
 					
@@ -112,6 +127,64 @@ public class IssuesActivity extends ListActivity {
 		return result;
 	}
 	
+	/** Methods for selected menu option. */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case SETTINGS_ID:
+			showDialog(NUMBER_DIALOG_ID);
+			return true;
+		case EXIT_ID:
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	protected Dialog onCreateDialog(int id) {
+	    Dialog dialog = null;
+	    switch(id) {
+	    case NUMBER_DIALOG_ID:
+	    	Dialog perpageDialog = new Dialog(this);
+	    	perpageDialog.setOnDismissListener(mNumberSetListener);
+	    	perpageDialog.setContentView(R.layout.perpage_dialog);
+	    	perpageDialog.setTitle("Number of issues per page");
+	    	EditText perpage = (EditText) perpageDialog.findViewById(R.id.perpage);
+	    	perpage.setText(String.valueOf(mPerPage));
+	    	Button okButton = (Button) perpageDialog.findViewById(R.id.perpage_ok);
+	    	okButton.setOnClickListener(new View.OnClickListener() {
+				// anonymous inner class
+				public void onClick(View view) {
+					dismissDialog(NUMBER_DIALOG_ID);
+				}
+			});
+	    	return perpageDialog;
+	    default:
+	        dialog = null;
+	    }
+	    return dialog;
+	}
+	
+	// the callback received when the user "sets" the number in the dialog
+	private DialogInterface.OnDismissListener mNumberSetListener =
+			new DialogInterface.OnDismissListener() {
+		@Override
+		public void onDismiss(DialogInterface dialog) {
+			Dialog perpageDialog = (Dialog)dialog;
+			EditText perpage = (EditText) perpageDialog.findViewById(R.id.perpage);
+			int newPerPage = 0;
+			try {
+				newPerPage = Integer.valueOf(perpage.getText().toString());
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+			newPerPage = (newPerPage > MAX_PER_PAGE) ? MAX_PER_PAGE : newPerPage;
+			mPerPage = (newPerPage < 1) ? 1 : newPerPage;
+			loadIssuesPage(mPage);
+		}
+	};
+
 	private void getIssues() {
 		JSONObject[] allIssues = null;
 		try {
@@ -170,18 +243,10 @@ public class IssuesActivity extends ListActivity {
 	}
 	
 	/** Intent for Project Details Activities. */
-	private void loadIssuesPage(EditText pageNumber) {
-		int newPage;
-		try {
-		newPage = Integer.valueOf(pageNumber.getText().toString());
-		} catch (NumberFormatException e) {
-			pageNumber.setText(String.valueOf(mPage));
-			e.printStackTrace();
-			return;
-		}
-		mPage = (newPage < mTotalPages) ? mPage : mTotalPages;
-		mPage = (newPage > 1) ? mPage : 1;
-		String pageURL = mIssuesURL + "?" + PER_PAGE + "=" + 25;
+	private void loadIssuesPage(int newPage) {
+		newPage = (newPage > mTotalPages) ? mTotalPages : newPage;
+		mPage = (newPage < 1) ? 1 : newPage;
+		String pageURL = mIssuesURL + "?" + PER_PAGE + "=" + mPerPage;
 		pageURL += "&" + GOTO_PAGE + "=" + mPage;
 		changePage(pageURL);
 	}
